3970c10af4a084f0caa780245e4643c71e6e256c
Pedro Cardoso <pedrohc.3108@gmail.com>
2026-05-20 22:40:17 -0300

    adicionei o schema do cliente

---
+++ src/clientes/infra/cliente.schema.ts
@@ -0,0 +1,20 @@
+    import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';//importa os decoradores necessários do NestJS para definir o esquema do Mongoose
+    import { Document } from 'mongoose';
+
+    export type ClienteDocument = ClienteModel & Document;//define o tipo do documento do cliente como a interseção entre o modelo do cliente e o documento do Mongoose
+
+    @Schema({ timestamps: true })//decorador para definir o esquema do Mongoose com a opção de timestamps para criar automaticamente os campos createdAt e updatedAt
+    export class ClienteModel {
+    @Prop({ required: true })
+    nome!: string;
+
+    @Prop({ required: true ,unique:true})
+    email!: string;
+
+    @Prop({ required: true ,unique:true})
+    phone!: string;
+
+    @Prop()
+    endereco?: string;
+    }
+    export const ClienteSchema = SchemaFactory.createForClass(ClienteModel);//cria o esquema do Mongoose a partir da classe do modelo do cliente usando a função SchemaFactory do NestJS
