41e4ca22b42fd5ad9565a04e0482d7ca4703867b
Pedro Cardoso <pedrohc.3108@gmail.com>
2026-06-06 13:53:19 -0300

    finalizações:exclusão dos comentarios e pequenos ajustes

--- src/clientes/clientes.controller.ts
+++ src/clientes/clientes.controller.ts
@@ -4,49 +4,49 @@ import { CreateClienteDto } from './dto/create-cliente.dto';
 import { UpdateClienteDto } from './dto/update-cliente.dto';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
-@ApiTags('Clientes')//decorador para agrupar as rotas relacionadas a clientes na documentação do Swagger
+@ApiTags('Clientes')
 @Controller('clientes')
 export class ClientesController {
   constructor(private readonly service: ClientesService) {}
 
-  @ApiOperation({ summary: 'Criar um novo cliente' })//decorador para adicionar uma descrição à rota no Swagger
+  @ApiOperation({ summary: 'Criar um novo cliente' })
   @Post()
   create(@Body() createClienteDto: CreateClienteDto) {
-    return this.service.create(createClienteDto);// Rota para criar um novo cliente
+    return this.service.create(createClienteDto);
   }
-  @ApiOperation({ summary: 'Obter todos os clientes' })//decorador para adicionar uma descrição à rota no Swagger
+  @ApiOperation({ summary: 'Obter todos os clientes' })
   @Get()
   findAll() {
     return this.service.findAll();
-  }// Rota para obter todos os clientes
+  }
   
-    @ApiOperation({ summary: 'Obter clientes por nome' })//decorador para adicionar uma descrição à rota no Swagger
+    @ApiOperation({ summary: 'Obter clientes por nome' })
     @Get('search/name/:nome')
     findByName(@Param('nome') nome: string) {
       return this.service.findByName(nome);
     }// Rota para obter clientes por nome
 
-    @ApiOperation({ summary: 'Obter um cliente por email' })//decorador para adicionar uma descrição à rota no Swagger
+    @ApiOperation({ summary: 'Obter um cliente por email' })
     @Get('search/email/:email')
     findByEmail(@Param('email') email: string) {
       return this.service.findByEmail(email);
     }// Rota para obter um cliente específico por email
-     @ApiOperation({ summary: 'Obter um cliente por ID' })//decorador para adicionar uma descrição à rota no Swagger
+     @ApiOperation({ summary: 'Obter um cliente por ID' })
      @Get(':id')
   findOne(@Param('id') id: string) {
     return this.service.findOne(id);// Rota para obter um cliente específico por ID
   } 
 
-    @ApiOperation({ summary: 'Atualizar um cliente por ID' })//decorador para adicionar uma descrição à rota no Swagger
+    @ApiOperation({ summary: 'Atualizar um cliente por ID' })
     @Put(':id')
     update(@Param('id') id: string, @Body() updateClienteDto: UpdateClienteDto) {
-    return this.service.update(id, updateClienteDto);// Rota para atualizar um cliente específico por ID
+    return this.service.update(id, updateClienteDto);
     }
-    @ApiOperation({ summary: 'Remover um cliente por ID' })//decorador para adicionar uma descrição à rota no Swagger
+    @ApiOperation({ summary: 'Remover um cliente por ID' })
     @Delete(':id')
     @HttpCode(204)
     remove(@Param('id') id: string) {
-    return this.service.remove(id);// Rota para remover um cliente específico por ID
+    return this.service.remove(id);
     }
     
 }
--- src/clientes/infra/cliente.schema.ts
+++ src/clientes/infra/cliente.schema.ts
@@ -1,9 +1,9 @@
     import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
     import { Document } from 'mongoose';
 
-    export type ClienteDocument = ClienteModel & Document;//define o tipo do documento do cliente como a interseção entre o modelo do cliente e o documento do Mongoose
+    export type ClienteDocument = ClienteModel & Document;
 
-    @Schema({ timestamps: true })//decorador para definir o esquema do Mongoose com a opção de timestamps para criar automaticamente os campos createdAt e updatedAt
+    @Schema({ timestamps: true })
     export class ClienteModel {
     @Prop({ required: true })
     nome!: string;
